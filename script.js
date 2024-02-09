211b1fd21bc89ff24092136f8be1d0a0d1f1a9f6
kirillhmk <kirillkirillhmk@gmail.com>
2024-02-09 23:22:48 +0200

    created array and function optimized by using bracket notation to call objects and values from array locations

--- script.js
+++ script.js
@@ -20,35 +20,40 @@ const monsterHealthText = document.querySelector("#monsterHealth");
 const locations = [
   {
     name: "town square",
-    "button text": ["Go to store", "Go to cave", "Fight dragon"],
-    "button functions": [goStore, goCave, fightDragon],
+    buttonText: ["Go to store", "Go to cave", "Fight dragon"],
+    buttonFunctions: [goStore, goCave, fightDragon],
     text: "You are in the town square. You see a sign that says \"Store\"."
+  },
+  {
+    name: "store",
+    buttonText: ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
+    buttonFunctions: [buyHealth, buyWeapon, goTown],
+    text: "You enter the store."
   }
 ];
+console.log(locations);
 
 //initialize buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
+function update(location) {
+  button1.innerText = location.buttonText[0];
+  button2.innerText = location.buttonText[1];
+  button3.innerText = location.buttonText[2];
+  button1.onclick = location.buttonFunctions[0];
+  button2.onclick = location.buttonFunctions[1];
+  button3.onclick = location.buttonFunctions[2];
+  text.innerText = location.text;
+}
+
 function goTown() {
-  button1.innerText = "Go to store";
-  button2.innerText = "Go to cave";
-  button3.innerText = "Fight dragon";
-  button1.onclick = goStore;
-  button2.onclick = goCave;
-  button3.onclick = fightDragon;
-  text.innerText = "You are in the town square. You see a sign that says \"Store\".";
+  update(locations[0]);
 }
 
 function goStore() {
-  button1.innerText = "Buy 10 health (10 gold)";
-  button2.innerText = "Buy weapon (30 gold)";
-  button3.innerText = "Go to town square";
-  text.innerText = "You enter the store.";
-  button1.onclick = buyHealth;
-  button2.onclick = buyWeapon;
-  button3.onclick = goTown;
+  update(locations[1]);
 }
 
 function goCave() {
@@ -57,4 +62,12 @@ function goCave() {
 
 function fightDragon() {
   console.log("Fighting dragon.");
+}
+
+function buyHealth() {
+
+}
+
+function buyWeapon() {
+
 }
